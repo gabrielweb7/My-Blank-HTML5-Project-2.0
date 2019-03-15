3e785bf6b841cafb981f82a5c1ccbf0bfbbb0a79
Gabriel da Luz <gabrielbarbosaweb7@gmail.com>
2019-03-15 03:54:39 -0400

    Inicio d Belezura

---
+++ js/site.js
@@ -0,0 +1,33 @@
+/**************************************************
+		
+  		 Desenvolvido por Gabriel da Luz 
+		
+		 http://gabrieldaluz.com.br/
+	
+***************************************************/
+
+
+/** Functions */
+
+function func() {
+	
+	return true;
+}
+
+
+/** Jquery Function */
+
+$(function() { 
+
+
+
+});
+
+
+
+/***************************************************
+*** Não remova os créditos pq eu vou saber! Õ_Õ. ***
+****************************************************/
+console.log(' ');
+console.log('%c~~ Desenvolvido por http://gabrieldaluz.com.br ~~ ','background:black;color:#00ff08; padding:6px 10px; margin:0px; border:4px solid gray; font-size:14px;');
+console.log(' ');
